33b4d233a9dd82903138de64b94935e7505d8baa
Alex Barron <s3831619@student.rmit.edu.au>
2023-01-11 23:13:41 +0800

    Scoring calc redrafted for exceptions errors

--- src/ScoreCalculator.cpp
+++ src/ScoreCalculator.cpp
@@ -1,110 +1,175 @@
 #include "../include/ScoreCalculator.h"
 
-ScoreCalculator::ScoreCalculator() {
-
+ScoreCalculator::ScoreCalculator()
+{
 }
 
-ScoreCalculator::~ScoreCalculator() {
-
+ScoreCalculator::~ScoreCalculator()
+{
 }
 
 // TODO ALEX B
-// Need to calculate the points of 1 for the first tile on the board
-// Test file only works whilst the tiles are unvalidated
+// Fix up max / min. Should come from the board
 // Comments need cleaning
 // Code needs reviewing
 // Assumes only valid tiles are added, for example possible to score 7
 
-int ScoreCalculator::calculateScore(Board* board, char row, int col) {
-
-    // zero points totals
+int ScoreCalculator::calculateScore(Board *board, char row, int col)
+{
     int totalPoints = 0;
-    int rowScore = 0;
-    int colScore = 0;
+    // Get Scores
+    int rowScore = getRowScore(board, row, col);
+    int colScore = getColScore(board, row, col);
 
-    // Check if first tile on the board
-    if ((board->getTileAtPos(row, col - 1) == nullptr && board->getTileAtPos(row, col + 1) == nullptr) && 
-        (board->getTileAtPos(row - 1, col) == nullptr && board->getTileAtPos(row + 1, col) == nullptr))
+    // Catches tiles with no adjacent tiles
+    if (colScore == 1 && rowScore == 1)
     {
         totalPoints = 1;
     }
-    
-
-    if (board->getTileAtPos(row, col - 1) != nullptr || board->getTileAtPos(row, col + 1) != nullptr)
+    // Catches tiles added to extend a row only
+    else if (colScore == 1 && rowScore > 1)
     {
-        // get score for row
-        rowScore = getRowScore(board, row, col); 
-        if(rowScore == 6) {
-            printQwirkle();
-            rowScore += 6;
-        }
-        // add row score to points
-        totalPoints = totalPoints + rowScore;
+        totalPoints = rowScore;
     }
-    
-    if (board->getTileAtPos(row - 1, col) != nullptr || board->getTileAtPos(row + 1, col) != nullptr)
+    // Catches tiles added to extend a column only
+    else if (colScore > 1 && rowScore == 1)
     {
-        // calculate col score
-        colScore = getColScore(board, row, col);
-        if(colScore == 6) {
-            printQwirkle();
-            colScore += 6;
-        }
-            
-        // add col score to points
-        totalPoints = totalPoints + colScore;
+        totalPoints = colScore;
+    }
+    // Catches tiles added which extend both a row and a column
+    else
+    {
+        totalPoints = rowScore + colScore;
     }
 
     return totalPoints;
 }
 
-int ScoreCalculator::getRowScore(Board* board, char row, int col) {
+int ScoreCalculator::getRowScore(Board *board, char row, int col)
+{
     int rowScore = 0;
-    int startCol = col;
+    // Get Start of Row Position, which column does it start at?
+    int rowStartPos = getRowStart(board, row, col);
+    // Get End of Row Position, which column does it end at?
+    int rowEndPos = getRowEnd(board, row, col);
 
-    // Go left
-    while (board->getTileAtPos(row, startCol) != nullptr)
-    {
-        startCol--;
-        rowScore++;
-        
-    }
-    // Go Right
-    // reset start col and add one to avoid double counting placed tile
-    startCol = col + 1;
-    while (board->getTileAtPos(row, startCol) != nullptr)
-    {
-        startCol++;
-        rowScore++;
-    }
+    // Calculate the row score by taking the start position from the end position
+    rowScore = rowEndPos - rowStartPos - 1;
+    // Check if its Qwirkle and if so add the points
+    rowScore += checkQwirklePoints(rowScore);
+
     return rowScore;
 }
 
-int ScoreCalculator::getColScore(Board* board, char row, int col) {
+int ScoreCalculator::getColScore(Board *board, char row, int col)
+{
     int colScore = 0;
-    int startRow = row;
+    // Get Start of Col Position, which row does it start at?
+    int colStartPos = getColStart(board, row, col);
+    // Get End of Col Position, which row does it end at?
+    int colEndPos = getColEnd(board, row, col);
 
-    // Go Up
-    while (board->getTileAtPos(startRow, col) != nullptr)
-    {
-        startRow--;
-        colScore++;
-    }
-
-    // Go Down
-    // reset start row and add one to avoid double counting placed tile
-    startRow = row + 1;
-    while (board->getTileAtPos(startRow, col) != nullptr)
-    {
-        startRow++;
-        colScore++;
-    }
+    // Calculate the col score by taking the start position from the end position
+    colScore = colEndPos - colStartPos - 1;
+    // Check if its Qwirkle and if so add the points
+    colScore += checkQwirklePoints(colScore);
 
     return colScore;
 }
 
+int ScoreCalculator::getRowStart(Board *board, char row, int col)
+{
+    int rowStartCol = col;
+    // Try / Catch in case of out of range exception for board
+    try
+    {
+        // Count back from the tile to find which location is not occupied by a tile
+        while (rowStartCol >= MIN_COL && board->getTileAtPos(row, rowStartCol) != nullptr)
+        {
+            rowStartCol--;
+        }
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 
-void ScoreCalculator::printQwirkle() const {
+    return rowStartCol;
+}
+
+int ScoreCalculator::getRowEnd(Board *board, char row, int col)
+{
+    int rowEndCol = col;
+    // Try / Catch in case of out of range exception for board
+    try
+    {
+        // Count forward from the tile to find which location is not occupied by a tile
+        while (rowEndCol <= MAX_COL && board->getTileAtPos(row, rowEndCol) != nullptr)
+        {
+            rowEndCol++;
+        }
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return rowEndCol;
+}
+
+int ScoreCalculator::getColStart(Board *board, char row, int col)
+{
+    int colStartRow = row;
+    // Try / Catch in case of out of range exception for board
+    try
+    {
+        // Count up from the tile to find which location is not occupied by a tile
+        while (colStartRow >= MIN_ROW && board->getTileAtPos(colStartRow, col) != nullptr)
+        {
+            colStartRow--;
+        }
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return colStartRow;
+}
+
+int ScoreCalculator::getColEnd(Board *board, char row, int col)
+{
+    int colEndRow = row;
+    // Try / Catch in case of out of range exception for board
+    try
+    {
+        // Count down from the tile to find which location is not occupied by a tile
+        while (colEndRow <= MAX_ROW && board->getTileAtPos(colEndRow, col) != nullptr)
+        {
+            colEndRow++;
+        }
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return colEndRow;
+}
+
+int ScoreCalculator::checkQwirklePoints(int score)
+{
+    int points = 0;
+    if (score == 6)
+    {
+        printQwirkle();
+        points = 6;
+    }
+    return points;
+}
+
+void ScoreCalculator::printQwirkle() const
+{
     std::cout << "QWIRKLE!!!" << std::endl;
     std::cout << std::endl;
 }
