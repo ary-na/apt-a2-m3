dc69db6662b0ceb3bab4e2d57d2aba73d0e205b0
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-01-17 10:48:35 +1100

    Delete Moves class functions from validator class

--- include/Validator.h
+++ include/Validator.h
@@ -2,10 +2,9 @@
 #define VALIDATOR_H
 
 #include <string>
-#include "Board.h"
-#include "LinkedList.h"
 #include <iostream>
 #include <fstream>
+#include <regex>
 
 class Validator {
 public:
@@ -13,46 +12,16 @@ public:
 public:
     Validator();
 
-    explicit Validator(Board *board);
-
     ~Validator();
 
-    const static int MAX_NUM_TILES = 6;
-    const static int MIN_START_COL = 0;
-    const static int MAX_START_COL = 25;
-    const static int MIN_START_ROW = 'A';
-    const static int MAX_START_ROW = 'Z';
-
-    // Validate username input (Players should only consist of letters (no numbers or symbols))
+    // Check if username is valid (Players should only consist of letters (no numbers or symbols))
     bool isNameValid(const std::string &input);
-
-    // Return a list of tiles added to the row
-    LinkedList *getRowTiles(char row, int col);
-
-    // Return a list of tiles added to the column
-    LinkedList *getColumnTiles(char row, int col);
-
-    // Check if the board is empty
-    bool isBoardEmpty();
-
-    // Check matching of tiles based on the colours and shapes
-    bool isTileColourMatch(const LinkedList* line, Tile *tile);
-
-    // Check matching of tiles based on the colours and shapes
-    bool isTileShapeMatch(const LinkedList* line, Tile *tile);
-
-    // Check if a tile already exists on the board
-    bool isTileExistAtLocation(char row, int col);
 
     // Check if saved file exist
     bool isSavedFileExist(std::string fileName);
 
-    bool isPlayerNameValid (std::string playerName);
+    // bool isPlayerNameValid (std::string playerName);
 
-private:
-    Board *board;
-    LinkedList *tileColumn;
-    LinkedList *tileRow;
 };
 
 #endif // VALIDATOR_H
