78fe89a62ffa4db7793fa07e2307eaf079227882
Alex Barron <s3831619@student.rmit.edu.au>
2023-01-21 20:00:01 +0800

    Updated testing and added Shell Script

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -11,39 +11,39 @@ make
 
 echo "Program Testing"
 
-echo "Menu → Select credits"
+echo "Testing: Menu → Select credits"
 ./qwirkle < tests/credits.input > tests/credits.gameout 
 diff -w -B tests/credits.output tests/credits.gameout 
 
-echo "Menu → Select new game"
+echo "Testing: Menu → Select new game"
 ./qwirkle T < tests/newGame.input > tests/newGame.gameout 
 diff -w tests/newGame.output tests/newGame.gameout 
 
-echo "Menu → Exit game"
+echo "Testing: Menu → Exit game"
 ./qwirkle < tests/quit.input > tests/quit.gameout 
 diff -w -B tests/quit.output tests/quit.gameout 
 
-echo "Top left corner place tiles"
+echo "Testing: Top left corner place tiles"
 ./qwirkle T < tests/corner1.input > tests/corner1.gameout 
 diff tests/corner1.output tests/corner1.gameout 
 
-echo "Top right corner place tiles"
+echo "Testing: Top right corner place tiles"
 ./qwirkle T < tests/corner2.input > tests/corner2.gameout 
 diff tests/corner2.output tests/corner2.gameout 
 
-echo "Bottom left corner place tiles"
+echo "Testing: Bottom left corner place tiles"
 ./qwirkle T < tests/corner3.input > tests/corner3.gameout 
 diff tests/corner3.output tests/corner3.gameout 
 
-echo "Bottom right corner place tiles"
+echo "Testing: Bottom right corner place tiles"
 ./qwirkle T < tests/corner4.input > tests/corner4.gameout 
 diff tests/corner4.output tests/corner4.gameout 
 
-echo "Qwirkle row scoring"
+echo "Testing: Qwirkle row scoring"
 ./qwirkle T < tests/qwirkleRow.input > tests/qwirkleRow.gameout 
 diff tests/qwirkleRow.output tests/qwirkleRow.gameout 
 
-echo "Qwirkle column scoring"
+echo "Testing: Qwirkle column scoring"
 ./qwirkle T < tests/qwirkleColumn.input > tests/qwirkleColumn.gameout 
 diff tests/qwirkleColumn.output tests/qwirkleColumn.gameout 
 
